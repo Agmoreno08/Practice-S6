737229305c879629dde5bede0e1e5258624f061c
Agmoreno08 <agmoreno@uamv.edu.ni>
2024-09-07 15:34:18 -0600

    Actualizaciones sesion 6

--- Sesion6/src/main/java/models/Faculty.java
+++ Sesion6/src/main/java/models/Faculty.java
@@ -1,5 +1,6 @@
 package Sesion6;
 
+import java.util.ArrayList;
 
 /**
  * @author GraceLowery
@@ -11,25 +12,50 @@ public class Faculty {
 	private String building;
 	private int id;
 	private String name;
-	public Cordination m_Cordination;
+	private ArrayList<Cordination> cordinationList;
 
-	public Faculty(){
-
+	// Constructor
+	public Faculty() {
+		cordinationList = new ArrayList<>();
 	}
 
+	public String getBuilding() {
+		return building;
+	}
+
+	public void setBuilding(String building) {
+		this.building = building;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	// Method to add a new Cordination
+	public void agregarCordination(int code, String name) {
+		Cordination newCordination = new Cordination(code, name);
+		cordinationList.add(newCordination);
+	}
+
+	// Method to return the list of Cordination objects
+	public ArrayList<Cordination> mostrarListasCordination() {
+		return cordinationList;
+	}
+
+	@Override
 	public void finalize() throws Throwable {
-
+		// Finalization logic if needed
 	}
-	/**
-	 * 
-	 * @param int
-	 * @param String
-	 */
-	public void agregarCordination(int int, String String){
-
-	}
-
-	public Arraylist<Cordination> mostrarListasCordination(){
-		return null;
-	}
-}//end Faculty
+}
--- Sesion6/src/main/java/models/Teacher.java
+++ Sesion6/src/main/java/models/Teacher.java
@@ -1,6 +1,5 @@
 package Sesion6;
 
-
 /**
  * @author GraceLowery
  * @version 1.0
@@ -12,24 +11,57 @@ public class Teacher {
 	private String lastname;
 	private String name;
 	private String qualification;
-	public Cordination m_Cordination;
+	private Cordination m_Cordination;
 
-	public Teacher(){
+	// Constructor
+	public Teacher() {
 
 	}
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getQualification() {
+		return qualification;
+	}
+
+	public void setQualification(String qualification) {
+		this.qualification = qualification;
+	}
+
+	// Getter for Cordination
+	public Cordination getCordination() {
+		return m_Cordination;
+	}
+
+	// Setter for Cordination
+	public void setCordination(Cordination cordination) {
+		this.m_Cordination = cordination;
+	}
+
+	@Override
 	public void finalize() throws Throwable {
-
+		// Finalization logic if needed
 	}
-	/**
-	 * 
-	 * @param Cotdination
-	 */
-	public void getCordination(Cordination Cotdination){
-
-	}
-
-	public void setCordination(){
-
-	}
-}//end Teacher
+}
